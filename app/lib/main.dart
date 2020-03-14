7c68b386c929aabff64d916e05c827e7f147f45c
hamzaali <hamza54834@gmail.com>
2020-03-14 04:52:27 -0700

    Simple widget tree implement in widget

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,7 +6,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
  Widget build(BuildContext context){
   //  @override
-       return MaterialApp(home: Text('Hello!'),);
+       return MaterialApp(home: Scaffold(
+         appBar: AppBar(title: Text('My first App'),
+         ),
+         body: Text('This is my text'),
+                                        //Ctrl+Space
+       ),
+       );
 
   }
 }
