109510fc521e379a02bebc60b3e71cb53beea016
hamzaali <hamza54834@gmail.com>
2020-03-14 04:38:04 -0700

    Simple widget create arrow function used

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
  Widget build(BuildContext context){
+  //  @override
        return MaterialApp(home: Text('Hello!'),);
   }
 }
