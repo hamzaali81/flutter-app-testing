89521986ce79b2cc628bfd8dfb1e4bd60c966b81
hamzaali <hamza54834@gmail.com>
2020-03-14 06:01:58 -0700

    Add buttons

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,16 +3,34 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget{
+  void answerQuestion(){
+    print('Answer question!');
+  }
+  @override
  Widget build(BuildContext context){
+   var questions=[
+     'What\'s is your favorite colour?',
+      'What\'s your favorite animal?'
+   ];
   //  @override
        return MaterialApp(home: Scaffold(
          appBar: AppBar(title: Text('My first App'),
          ),
-         body: Text('This is my text'),
+        //  body: Text('This is my text'),
+        body: Column(
+          children:
+          //  <Widget>[
+          [
+            Text('The question'),
+            RaisedButton(child: Text('Answer 1'),onPressed: null,),
+            RaisedButton(child: Text('Answer 2'),onPressed: null),
+            RaisedButton(child: Text('Answer 3'),onPressed: null),
+           ],
+           ),
                                         //Ctrl+Space
        ),
        );
-
-  }
+}
 }
